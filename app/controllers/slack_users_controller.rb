5a2ea73cc2e29a235350109b1ea51445a0761bb8
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-04-27 16:18:54 -0600

    Add actual style to the views
    I put the user info into a partial
    Added bootstrap gem
    Styled up every page in a minimalistic way
    Added buttons to add and remove users from lunch roulette group
    More navigation buttons

--- app/controllers/slack_users_controller.rb
+++ app/controllers/slack_users_controller.rb
@@ -1,9 +1,20 @@
 class SlackUsersController < ApplicationController
 
+  def index
+    @slack_users = SlackUser.all
+  end
+
   def destroy
     slack_user = SlackUser.find(params[:id])
     slack_user.update_attribute(:active, false)
     flash[:success] = "You have removed #{slack_user.name} from lunch roulette."
+    redirect_to user_group_path(slack_user.user_group_id)
+  end
+
+  def update
+    slack_user = SlackUser.find(params[:id])
+    slack_user.update_attribute(:active, true)
+    flash[:success] = "You have added #{slack_user.name} from lunch roulette."
     redirect_to user_group_path(slack_user.user_group_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,9 @@ Rails.application.routes.draw do
   delete '/logout',                  to: 'sessions#destroy',            as: :logout
   get    '/user_groups',             to: 'user_groups#index',           as: 'user_groups'
   resources :user_groups, only: [:index, :show]
-  resources :slack_users, only: [:index, :destroy]
+  resources :slack_users, only: [:index]
+  get    '/slack_users/remove/:id',  to: 'slack_users#destroy'
+  get    '/slack_users/add/:id',     to: 'slack_users#update'
   get    '/lunch_roulette_groups',   to: 'lunch_roulette_groups#index', as: 'lunch_roulette_groups', formats: {default: :json}
   get    '/:username',               to: 'users#show',                  as: 'user'
   get    '/:username/edit',          to: 'users#edit',                  as: 'edit_user'
