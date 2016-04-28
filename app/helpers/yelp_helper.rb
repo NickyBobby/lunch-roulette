2e1fbc4550dd91893efcaf025195c564d8ec7765
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-04-27 21:04:25 -0600

    Add more info from restaurant recommendations
    I added ratings and addresses to the output
    I used a new Restaurant class to make the object handling a little easier
    Refactored a little bit of code

--- app/helpers/yelp_helper.rb
+++ app/helpers/yelp_helper.rb
@@ -1,11 +1,17 @@
 module YelpHelper
-  def get_restaurant_names(restaurants)
-    restaurant_names = []
-    restaurants.each do |restaurant|
-      restaurant_names << "* " + restaurant.name
+  def get_restaurant_info(restaurant_params)
+    restaurants = []
+    restaurants = restaurant_params.map do |restaurant|
+      restaurants << Restaurant.new(restaurant)
     end
-    restaurants = restaurant_names.shuffle.shift(10)
-    {text: restaurants.join("\n")}
+    restaurant_results = get_restaurant_results(restaurants.first)
+    {text: restaurant_results.join("\n")}
+  end
+
+  def get_restaurant_results(restaurants)
+    restaurants.map do |restaurant|
+      "* #{restaurant.name} - #{restaurant.rating} Rating - #{restaurant.address.join(" ")}"
+    end.shuffle.shift(10)
   end
 
 end
