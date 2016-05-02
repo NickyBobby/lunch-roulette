165f15cf3310e31bd88ff5c6c2ef97e6bb2402e5
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-05-02 11:06:29 -0600

    changed lunch roulette group back to render json

--- app/controllers/api/v1/lunch_roulette_groups_controller.rb
+++ app/controllers/api/v1/lunch_roulette_groups_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::LunchRouletteGroupsController < Api::V1::BaseController
-  respond_to :json
+  # respond_to :json
 
   def index
-    respond_with LunchRouletteGroup.generate_lunch_roulette_groups
+    # respond_with LunchRouletteGroup.generate_lunch_roulette_groups
+    render json: LunchRouletteGroup.generate_lunch_roulette_groups
   end
 end
